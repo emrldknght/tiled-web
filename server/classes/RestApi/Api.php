e48704206a184ac8eddeaa0045a10cdcd0b279de
emkni <vhhl@list.ru>
2021-07-28 21:11:51 +0300

    server - Api - init

---
+++ server/classes/RestApi/Api.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace SteamQ\RestApi;
+
+class Api {
+
+    public static ?User $User = null;
+
+    public static function processPath(): array
+    {
+        $parsed_url = parse_url($_SERVER['REQUEST_URI']);
+        $path = $parsed_url['path'];
+        $pathArr = [];
+        if (!empty($path)) {
+            $pathArr = explode('/', $path);
+            $pathArr = array_filter($pathArr, function ($el) {
+                return !empty($el);
+            });
+            $pathArr = array_values($pathArr);
+        }
+        return $pathArr;
+    }
+    public static array $paths = [
+        'GET' => [],
+        'POST' => []
+    ];
+
+    public static array $defaultRestrictions = [
+        'auth' => false,
+        'admin' => false
+    ];
+
+    public static function hasAction(string $name, string $type): bool
+    {
+        return array_key_exists($name, self::$paths[$type]);
+    }
+
+    public static function get(string $name, callable $cb, array $restrictions = [])
+    {
+        self::addAction($name, $cb, $restrictions, 'GET');
+    }
+
+    public static function post(string $name, callable $cb, array $restrictions = [])
+    {
+        self::addAction($name, $cb, $restrictions, 'POST');
+    }
+
+    public static function addAction(string $name, callable $cb, array $restrictions = [], string $type = 'get')
+    {
+        $restrictions = array_merge(self::$defaultRestrictions, $restrictions);
+        self::$paths[$type][$name] = [
+            'cb' => $cb,
+            'rest' => $restrictions,
+            'type' => 'post'
+        ];
+    }
+
+    public static function callAction(string $action, string $type)
+    {
+        // global $USER;
+        $data = [];
+
+        // print_r(self::$paths);
+        if (!self::hasAction($action, $type)) {
+            $error = "API: action $action not supported!";
+            $data['error'] = $error;
+            // print_r($error);
+            return $data;
+        }
+
+        $handler = self::$paths[$type][$action];
+
+        $restrictions = $handler['rest'];
+        if ($restrictions['auth']) {
+            // var_dump('checking auth');
+            if (!self::$User->IsAuthorized()) {
+                $data['error'] = 'Not authorized!';
+                return $data;
+            }
+        }
+        if ($restrictions['admin']) {
+            // var_dump('checking admin');
+            if (!self::$User->IsAdmin()) {
+                $data['error'] = 'Insufficient rights!';
+                return $data;
+            }
+        }
+        return $handler['cb']();
+    }
+}
