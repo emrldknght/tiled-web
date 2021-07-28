638748478f3bfc907031113137f593cc11b913e3
Andrew K <vhhl@list.ru>
2021-07-28 14:52:46 +0300

    ToolPalette - Component

--- src/components/MapEditorComponent.tsx
+++ src/components/MapEditorComponent.tsx
@@ -3,12 +3,14 @@ import {MapComponent} from "./MapComponent";
 import {CellPalette} from "./CellPalette";
 import {MapDimensionsComponent} from "./map/MapDimensions";
 import React from "react";
+import {ToolPalette} from "./ToolPalette";
 
 export const MapEditorComponent = observer(function MapEditorComponent() {
   return (
     <div className="row">
       <MapComponent/>
       <div className="col">
+        <ToolPalette />
         <CellPalette/>
         <MapDimensionsComponent />
       </div>
---
+++ src/components/ToolPalette.tsx
@@ -0,0 +1,60 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFill, faPen} from "@fortawesome/free-solid-svg-icons";
+import React, {createContext, useContext} from "react";
+import {observer} from "mobx-react";
+import {action, makeObservable, observable} from "mobx";
+
+export enum PalTools {
+    Pencil = 'pencil',
+    Bucket = 'bucket'
+}
+export type PalToolType = keyof typeof PalTools;
+
+export class ToolPaletteState {
+    @observable currentTool: PalToolType = 'Pencil'
+
+    @action changeTool(tool: PalToolType) {
+        this.currentTool = tool;
+    }
+
+    constructor() {
+        makeObservable(this)
+    }
+}
+export const toolPaletteState = new ToolPaletteState();
+export const ToolPaletteContext = createContext<ToolPaletteState>(toolPaletteState);
+
+export const ToolPalette = observer(function ToolPalette() {
+    const state = useContext(ToolPaletteContext);
+    const at = state.currentTool;
+
+    const handleChange = (e: React.MouseEvent) => {
+        const t = e.currentTarget as HTMLDivElement;
+        const tool = t.dataset.tool;
+        console.log(tool);
+        if(tool) {
+            state.changeTool(tool as PalToolType);
+        }
+    }
+
+    return(
+        <div className="tools-palette">
+            <b>Tools:</b>
+            <div className="row">
+                <div className={`tool-icon ${(at === 'Pencil') ? 'active' : ''}`} data-tool={'Pencil'}
+                     onClick={handleChange}
+
+                >
+                    <FontAwesomeIcon icon={faPen} size="2x"/>
+                    <span className="name">Pencil</span>
+                </div>
+                <div className={`tool-icon ${(at === 'Bucket') ? 'active' : ''}`} data-tool={'Bucket'}
+                     onClick={handleChange}
+                >
+                    <FontAwesomeIcon icon={faFill} size="2x"/>
+                    <span className="name">Bucket</span>
+                </div>
+            </div>
+        </div>
+    )
+})
