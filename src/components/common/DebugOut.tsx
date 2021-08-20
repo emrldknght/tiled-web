a022fd3355c77a37e253345376e5b546fd01418b
Andrew K <vhhl@list.ru>
2021-08-20 10:06:53 +0300

    DebugOut - added observer

--- src/components/common/DebugOut.tsx
+++ src/components/common/DebugOut.tsx
@@ -1,7 +1,9 @@
-export function DebugOut<T>({data}: {data: T}) {
+import {observer} from "mobx-react";
+
+export const DebugOut = observer(function DebugOut<T>({data}: {data: T}) {
   return (
     <div className="debug-out">
       {JSON.stringify(data)}
     </div>
   )
-}
+})
